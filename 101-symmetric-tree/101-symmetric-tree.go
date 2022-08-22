a27e720ef2af035403ce3818639c1effe2afcc3c
jonghyeons <whdgus906@gmail.com>
2022-08-22 21:18:41 +0900

    Time: 0 ms (100.00%), Space: 2.9 MB (17.73%) - LeetHub

---
+++ 101-symmetric-tree/101-symmetric-tree.go
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	return isSymmetricTree(root.Left, root.Right)
+}
+
+func isSymmetricTree(left *TreeNode, right *TreeNode) bool {
+	if right == left {
+		return true
+	}
+	if left == nil || right == nil || left.Val != right.Val {
+		return false
+	}
+	return isSymmetricTree(left.Left, right.Right) && isSymmetricTree(left.Right, right.Left)
+}
