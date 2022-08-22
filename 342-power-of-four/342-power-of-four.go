10ad46effc86d12a9507b4f6adf660dba15db251
jonghyeons <whdgus906@gmail.com>
2022-08-22 20:39:14 +0900

    Time: 0 ms (100.00%), Space: 2 MB (100.00%) - LeetHub

---
+++ 342-power-of-four/342-power-of-four.go
@@ -0,0 +1,15 @@
+func isPowerOfFour(n int) bool {
+	if n == 0 {
+		return false
+	}
+
+	if n == 1 {
+		return true
+	}
+
+	if n%4 == 0 {
+		return isPowerOfFour(n / 4)
+	} else {
+		return false
+	}
+}
