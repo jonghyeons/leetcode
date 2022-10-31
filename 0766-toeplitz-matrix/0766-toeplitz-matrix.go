37563cf23855f5fbad3301802bc915052572a56e
jonghyeons <whdgus906@gmail.com>
2022-10-31 20:05:53 +0900

    Time: 14 ms (76.47%), Space: 4.2 MB (85.29%) - LeetHub

---
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.go
@@ -0,0 +1,12 @@
+func isToeplitzMatrix(matrix [][]int) bool {
+	m := len(matrix)
+	n := len(matrix[0])
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			if matrix[i-1][j-1] != matrix[i][j] {
+				return false
+			}
+		}
+	}
+	return true
+}
