9aba5ae9de602bc1724d569e4953743869459e1b
jonghyeons <whdgus906@gmail.com>
2023-02-26 15:31:20 +0900

    Time: 3 ms (46.19%), Space: 2.8 MB (34.53%) - LeetHub

---
+++ 0046-permutations/0046-permutations.go
@@ -0,0 +1,20 @@
+var res [][]int
+
+func permute(nums []int) [][]int {
+    res = [][]int{}
+    dfs([]int{}, nums)
+	return res
+}
+
+func dfs(prev, nums []int) {
+	if len(nums) == 0 {
+		res = append(res, prev)
+		return
+	}
+    
+	for i, v := range nums {
+		element := make([]int, len(nums))
+		copy(element, nums)
+		dfs(append(prev, v), append(element[:i], element[i+1:]...))
+	}
+}
