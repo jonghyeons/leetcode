da14fd43814471d8cde94b07a605bbc529765708
jonghyeons <whdgus906@gmail.com>
2023-04-10 21:52:44 +0900

    Time: 0 ms (100.00%), Space: 2.3 MB (11.33%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.go
@@ -0,0 +1,37 @@
+func isValid(s string) bool {
+   	if len(s)%2 != 0 {
+		return false
+	}
+
+	check := map[interface{}]string{
+		"{": "}",
+		"[": "]",
+		"(": ")",
+	}
+    
+    res := false
+	stack := list.New()
+	for i := range s {
+		if _, exist := check[string(s[i])]; exist {
+			stack.PushFront(string(s[i]))
+			continue
+		} else {
+            if stack.Len() == 0 {
+				return false
+			}
+			if check[stack.Front().Value] == string(s[i]) {
+				stack.Remove(stack.Front())
+                res = true
+				continue
+			} else {
+				return false
+			}
+		}
+	}
+
+	if stack.Len() != 0 {
+		return false
+	} else {
+		return res
+	}
+}
