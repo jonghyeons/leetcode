cf21dbd1ffb8c80bdd26027944320b8a7f79487a
jonghyeons <whdgus906@gmail.com>
2023-04-11 21:27:56 +0900

    Time: 0 ms (100.00%), Space: 3.9 MB (84.80%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.go
@@ -0,0 +1,29 @@
+func numIslands(grid [][]byte) int {
+	res := 0
+	for x, v := range grid {
+		for y := range v {
+			if dfs(x, y, grid) {
+				res += 1
+			}
+		}
+	}
+	return res
+}
+
+func dfs(x, y int, grid [][]byte) bool {
+	if x < 0 || y < 0 || x >= len(grid) || y >= len(grid[0]) {
+		return false
+	}
+
+	if grid[x][y] == '1' {
+		grid[x][y] = '0'
+
+		dfs(x-1, y, grid)
+		dfs(x+1, y, grid)
+		dfs(x, y-1, grid)
+		dfs(x, y+1, grid)
+
+		return true
+	}
+	return false
+}
