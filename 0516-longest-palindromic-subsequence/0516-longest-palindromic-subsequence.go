492909baaca4c5ad10aaa3a3d12e15e9a102ef16
jonghyeons <whdgus906@gmail.com>
2023-04-16 20:26:32 +0900

    Time: 52 ms (49.40%), Space: 19.3 MB (52.61%) - LeetHub

---
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.go
@@ -0,0 +1,25 @@
+func longestPalindromeSubseq(s string) int {
+    n := len(s)
+    dp := make([][]int, n)
+    for i := range dp {
+        dp[i] = make([]int, n)
+        dp[i][i] = 1
+    }
+    for i := n - 2; i >= 0; i-- {
+        for j := i + 1; j < n; j++ {
+            if s[i] == s[j] {
+                dp[i][j] = dp[i+1][j-1] + 2
+            } else {
+                dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+            }
+        }
+    }
+    return dp[0][n-1]
+}
+
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
