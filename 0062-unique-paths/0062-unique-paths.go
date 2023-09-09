95d935e5b17a10c127145691370b75ea6455a1a5
jonghyeons <whdgus906@gmail.com>
2023-09-09 16:11:37 +0900

    Time: 1 ms (79.50%), Space: 2.1 MB (42.99%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.go
@@ -0,0 +1,22 @@
+func uniquePaths(m int, n int) int {
+	grid := make([][]int, m)
+	for i := range grid {
+		grid[i] = append(grid[i], make([]int, n)...)
+	}
+
+	for i := 0; i < m; i++ {
+		grid[i][0] = 1
+	}
+
+	for i := 0; i < n; i++ {
+		grid[0][i] = 1
+	}
+
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			grid[i][j] = grid[i-1][j] + grid[i][j-1]
+		}
+	}
+
+	return grid[m-1][n-1]
+}
